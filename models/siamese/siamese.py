1a3d12d55168c247580c32916de39c899e5f719b
vitalwarley <vitalwarley@gmail.com>
2020-07-02 13:02:39 -0300

    Models: refactors importing, and model definition

--- models/siamese/siamese.py
+++ models/siamese/siamese.py
@@ -1,26 +1,33 @@
-from tensorflow.keras import backend as K
+"""Siamese net.
+"""
 from tensorflow.keras import layers
 from tensorflow.keras import models
 
-from models.distances import euclidean_distance, eucl_dist_output_shape
+from models.distances import euclidean_distance, get_output_shape
 
 
-def create_base_siamese(input_shape):
-    input = layers.Input(shape=input_shape)
-    x = layers.Conv2D(64, 10, activation='relu')(input)
-    x = layers.MaxPooling2D()(x)
-    x = layers.Conv2D(128, 7, activation='relu')(x)
-    x = layers.MaxPool2D()(x)
+def create_base_siamese(input_shape=None, **kwargs):
+    # pylint: disable=unused-argument
+    """Base siamese net.
+    """
+    inputs = layers.Input(shape=input_shape)
+    out = layers.Conv2D(64, 10, activation='relu')(inputs)
+    out = layers.MaxPooling2D()(out)
+    out = layers.Conv2D(128, 7, activation='relu')(out)
+    out = layers.MaxPool2D()(out)
     # x = layers.Conv2D(128, 4, activation='relu')(x)
     # x = layers.MaxPool2D()(x)
     # x = layers.Conv2D(256, 4, activation='relu')(x)
-    x = layers.Flatten()(x)
-    embeddings = layers.Dense(4096, activation='sigmoid')(x)
-    model = models.Model(inputs=input, outputs=embeddings)
+    out = layers.Flatten()(out)
+    embeddings = layers.Dense(4096, activation='sigmoid')(out)
+    model = models.Model(inputs=inputs, outputs=embeddings)
     return model
 
 
-def create_siamese_network(input_shape):
+def create_siamese_network(input_shape=None, **kwargs):
+    # pylint: disable=unused-argument
+    """Siamese network for input pairs.
+    """
     base_network = create_base_siamese(input_shape)
 
     left_input = layers.Input(shape=input_shape)
@@ -31,11 +38,9 @@ def create_siamese_network(input_shape):
     embeddings = [left_embeddings, right_embeddings]
 
     distance = layers.Lambda(euclidean_distance,
-                             output_shape=eucl_dist_output_shape)(
+                             output_shape=get_output_shape)(
                                  embeddings)
 
-    # prediction = layers.Dense(1, activation='sigmoid')(distance)
-    # model = models.Model([left_input, right_input], prediction)
     model = models.Model([left_input, right_input], distance)
 
     return model
